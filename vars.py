0d45be44e29b179d882b026a140548002aad5d9a
Ajit Kumar <barupalajit40@gmail.com>
2025-12-28 11:29:24 +0530

    Update API credentials and retain comments

--- vars.py
+++ vars.py
@@ -4,7 +4,8 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "34156893"))
+API_HASH = environ.get("API_HASH", "f40061f9c273fed4a35bc061a2725e9d")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
+
 
